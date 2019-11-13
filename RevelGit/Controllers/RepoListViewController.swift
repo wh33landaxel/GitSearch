2da126611f31c2042d0bf23ea5be410ba5757442
Axel Nunez <axel.z.nunez@gmail.com>
2019-11-13 12:06:57 -0500

    Repos getting back proper data

--- RevelGit/Controllers/RepoListViewController.swift
+++ RevelGit/Controllers/RepoListViewController.swift
@@ -10,21 +10,48 @@ import UIKit
 
 class RepoListViewController: UIViewController {
 
+    @IBOutlet weak var repoTableView: UITableView!
+    var repoDetails : [RepositoryDetails] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Repo List"
 
         // Do any additional setup after loading the view.
-    }
+        ApolloManager.instance.findRepos(searchData: FindReposQuery(login: "Dave", last: 100)) { (result) in
+            
+            switch(result) {
+                            case .success(let graphQLResult):
+                                if let repos = graphQLResult.data?.user?.repositories.nodes {
+                                    
+                                    for repo in repos {
+                                        if let repoInfo = repo?.fragments.repositoryDetails
+                                        {
+                                            self.repoDetails.append(repoInfo)
+                                        }
+                                    }
+                                }
+                                self.repoTableView.reloadData()
+                            
+                            case .failure(let error):
+                                self.repoTableView.reloadData()
+                                print(error)
+                        }
+                    }
+                }
 }
+
 
 extension RepoListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return repoDetails.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.textLabel?.text = repoDetails[indexPath.row].name
+        
+        return cell
     }
 }
